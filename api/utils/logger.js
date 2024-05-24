999979531bd34e274d08df22e0ad3922213f1233
ibrahim11elian <ibrahim11elian@gmail.com>
2024-05-24 15:13:53 +0300

    config logger function from winston for logging

---
+++ api/utils/logger.js
@@ -0,0 +1,12 @@
+import { createLogger, format, transports } from 'winston';
+
+const logger = createLogger({
+  level: 'info',
+  format: format.combine(
+    format.timestamp(),
+    format.json(), // Output logs in JSON format
+  ),
+  transports: [new transports.File({ filename: 'app.log' })],
+});
+
+export default logger;
