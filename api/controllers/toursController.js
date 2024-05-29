ae5008ac51dee79299c397ad3a486fb65240f80f
ibrahim11elian <ibrahim11elian@gmail.com>
2024-05-29 19:02:39 +0300

    add geo controllers

--- api/controllers/toursController.js
+++ api/controllers/toursController.js
@@ -1,4 +1,5 @@
 import TourModel from '../models/tourModel.js';
+import AppError from '../utils/error.js';
 import BaseController from './baseController.js';
 
 class Tours extends BaseController {
@@ -96,6 +97,85 @@ class Tours extends BaseController {
     req.query.limit = '5';
     req.query.sort = 'price,-ratingsAverage';
     next();
+  };
+
+  getToursWithin = async (req, res, next) => {
+    try {
+      const { distance, latlng, unit } = req.params;
+      const [lat, lng] = latlng.split(',');
+
+      // mongo spacial wants the distance divided by the radius of the earth
+      const radius = unit === 'mi' ? distance / 3963.2 : distance / 6378.1;
+      if (!lat || !lng || !distance) {
+        next(
+          new AppError(
+            'Please provide latitude, longitude, and distance in the format lat,lng,distance',
+            400,
+          ),
+        );
+      }
+
+      const tours = await TourModel.find({
+        startLocation: { $geoWithin: { $centerSphere: [[lng, lat], radius] } },
+      });
+      res.status(200).json({
+        status: 'success',
+        results: tours.length,
+        data: tours,
+      });
+    } catch (error) {
+      next(error);
+    }
+  };
+
+  getDistances = async (req, res, next) => {
+    try {
+      const { latlng, unit } = req.params;
+      const [lat, lng] = latlng.split(',');
+
+      const multiplier = unit === 'mi' ? 0.000621371 : 0.001;
+
+      if (!lat || !lng) {
+        next(
+          new AppError(
+            'Please provide latitude, longitude, in the format lat,lng',
+            400,
+          ),
+        );
+      }
+
+      const distances = await TourModel.aggregate([
+        {
+          $geoNear: {
+            near: {
+              type: 'Point',
+              coordinates: [lng * 1, lat * 1],
+            },
+            distanceField: 'distance',
+            distanceMultiplier: multiplier,
+            spherical: true,
+          },
+        },
+        {
+          $project: {
+            distance: 1,
+            name: 1,
+          },
+        },
+        {
+          $sort: {
+            distance: 1,
+          },
+        },
+      ]);
+
+      res.status(200).json({
+        status: 'success',
+        data: distances,
+      });
+    } catch (error) {
+      next(error);
+    }
   };
 }
 
