4a596339018f7c3ad4ef6ae688067f8ba0c7f58c
ibrahim11elian <ibrahim11elian@gmail.com>
2024-05-29 19:02:09 +0300

    add check review middleware to prevent user from updating or deleting other user's reviews

--- api/controllers/reviewController.js
+++ api/controllers/reviewController.js
@@ -1,4 +1,5 @@
 import ReviewModel from '../models/reviewModel.js';
+import AppError from '../utils/error.js';
 import BaseController from './baseController.js';
 
 export class ReviewController extends BaseController {
@@ -17,6 +18,19 @@ export class ReviewController extends BaseController {
     // a middleware to manipulate the query to that will be handled on the get all (APIFeatures)
     if (req.params.tourId) {
       req.query.tour = req.params.tourId;
+    }
+    next();
+  };
+
+  checkReviewOwner = async (req, res, next) => {
+    // NOT FINISHED!
+    // middleware to check if the user performing update or delete on review is the owner of the review or not
+    const review = await this.model.findById(req.params.id);
+    if (!review) return next(new AppError('No review found with that ID', 404));
+    if (review.user.id.toString() !== req.user.id.toString()) {
+      return next(
+        new AppError('You are not authorized to perform this action', 403),
+      );
     }
     next();
   };
