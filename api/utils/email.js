75e17245da1a70792e8e430c7ddcc1616a2e5ba3
ibrahim11elian <ibrahim11elian@gmail.com>
2024-06-01 00:25:57 +0300

    refactor email class to be able to send welcome and reset password emails in dev and production environment.

--- api/utils/email.js
+++ api/utils/email.js
@@ -1,12 +1,31 @@
 /* eslint-disable no-undef */
+import path from 'path';
 import nodemailer from 'nodemailer';
+import pug from 'pug';
+import { htmlToText } from 'html-to-text';
+import AppError from './error.js';
+import logger from './logger.js';
 
 class Email {
-  constructor() {}
+  constructor(user, url) {
+    this.to = user.email;
+    this.firstName = user.name.split(' ')[0];
+    this.url = url;
+    this.from = `Booking Application <${process.env.EMAIL_FROM}>`;
+  }
 
-  sendEmail = async (options) => {
-    //  create transporter
-    const transporter = nodemailer.createTransport({
+  createNewTransport() {
+    if (process.env.NODE_ENV === 'production') {
+      return nodemailer.createTransport({
+        service: 'SendGrid',
+        auth: {
+          user: process.env.SENDGRID_USERNAME,
+          pass: process.env.SENDGRID_PASSWORD,
+        },
+      });
+    }
+
+    return nodemailer.createTransport({
       host: process.env.EMAIL_HOST,
       port: process.env.EMAIL_PORT,
       auth: {
@@ -14,16 +33,48 @@ class Email {
         pass: process.env.EMAIL_PASSWORD,
       },
     });
-    // define the email options
-    const mailOptions = {
-      from: 'ibrahim ahmed <ibrahim@mail.com>',
-      to: options.email,
-      subject: options.subject,
-      text: options.message,
-    };
+  }
 
-    // send email
-    await transporter.sendMail(mailOptions);
+  send = async (template, subject) => {
+    try {
+      // render HTML based on a bug template
+      const html = pug.renderFile(
+        path.join(process.cwd(), 'views', `${template}.pug`),
+        {
+          firstName: this.firstName,
+          url: this.url,
+          subject,
+        },
+      );
+      // define the email options
+      const mailOptions = {
+        from: this.from,
+        to: this.to,
+        subject,
+        html,
+        text: htmlToText(html),
+      };
+
+      //  create transporter
+      const transporter = this.createNewTransport();
+
+      // send email
+      await transporter.sendMail(mailOptions);
+    } catch (error) {
+      logger.error(`Error sending email to user ${this.to}:`, error);
+      throw new AppError('Error Sending email');
+    }
+  };
+
+  sendWelcome = async () => {
+    await this.send('welcome', 'welcome to the tours family');
+  };
+
+  sendResetPassword = async () => {
+    await this.send(
+      'passwordReset',
+      'Booking App Account Reset Password (valid for 10 min)',
+    );
   };
 }
 
