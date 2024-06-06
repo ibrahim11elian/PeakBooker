1aac4348f7c3f539acebe2975c88f22522da0e84
ibrahim11elian <ibrahim11elian@gmail.com>
2024-06-06 21:17:27 +0300

    add tour date field to the booking model and method that calc the number of participants that booked the tour on specific date

--- api/models/bookingModel.js
+++ api/models/bookingModel.js
@@ -1,4 +1,5 @@
 import mongoose from 'mongoose';
+import TourModel from './tourModel.js';
 
 const bookingSchema = mongoose.Schema(
   {
@@ -25,6 +26,10 @@ const bookingSchema = mongoose.Schema(
       type: Boolean,
       default: false,
     },
+    tourDate: {
+      type: Date,
+      required: [true, 'Booking must have a tour date'],
+    },
   },
   {
     toJSON: { virtuals: true },
@@ -44,6 +49,54 @@ bookingSchema.pre(/^find/, function (next) {
   });
 
   next();
+});
+
+bookingSchema.statics.calcTourDateParticipants = async function (tourId) {
+  const stats = await this.aggregate([
+    {
+      $match: { tour: tourId },
+    },
+    {
+      $group: {
+        _id: '$tourDate',
+        participants: { $sum: 1 },
+      },
+    },
+  ]);
+
+  const tour = await TourModel.findById(tourId);
+  if (stats.length > 0) {
+    tour.startDates.forEach((date, i) => {
+      const participants =
+        stats.find((s) => s._id.toString() === date.toString())?.participants ||
+        0;
+      tour.participants[i] = participants;
+      tour.soldOut[i] = participants >= tour.maxGroupSize;
+    });
+  } else {
+    tour.participants = Array(tour.startDates.length).fill(0);
+    tour.soldOut = Array(tour.startDates.length).fill(false);
+  }
+  await tour.save();
+};
+
+bookingSchema.post('save', function () {
+  this.constructor.calcTourDateParticipants(this.tour);
+});
+
+bookingSchema.pre(/^findOneAnd/, async function (next) {
+  // pass the date from pre to post middleware
+  // reference the model to fetch the document before the update operation.
+  this.r = await this.model.findOne(this.getQuery()).select([]);
+  next();
+});
+
+bookingSchema.post(/^findOneAnd/, async function () {
+  // Use the document retrieved in pre middleware to update the average ratings
+
+  if (this.r) {
+    await this.r.constructor.calcTourDateParticipants(this.r.tour._id);
+  }
 });
 
 const Booking = mongoose.model('Booking', bookingSchema);
